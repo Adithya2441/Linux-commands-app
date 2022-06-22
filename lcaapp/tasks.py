1be9e538ec95b54f89273bb4452a39c054641434
Adithya S <84130203+Adithya2441@users.noreply.github.com>
2022-06-22 17:08:10 +0530

    celery updates

--- lcaapp/tasks.py
+++ lcaapp/tasks.py
@@ -1,8 +1,14 @@
 from __future__ import absolute_import,unicode_literals
-import re
+from celery import Celery, shared_task
+import time,subprocess
 
-from celery import shared_task
+app = Celery('tasks', broker='redis://localhost:6379',include=['problem.tasks.add'])
 
 @shared_task
-def add(x,y):
-    return x+y
+def celery_run(cmd,rep,dur):
+    output=''
+    for i in range(int(rep)):
+        time.sleep(int(dur))
+        p=subprocess.run(cmd,capture_output=True,text=True,shell=True)
+        output += p.stdout
+    return output
--- lcaapp/views.py
+++ lcaapp/views.py
@@ -1,19 +1,31 @@
-from threading import Timer
-import subprocess
-from time import sleep
+import time,subprocess
+from .tasks import celery_run
 from django.shortcuts import render
 
 def home(request):
-    if request.method == 'POST':
+    if 'mainform' in request.POST:
         command = request.POST.get('command')
         rep = request.POST.get('rep')
         dur = request.POST.get('dur')
         cmd ='powershell -command '+command
-        output=''
-        for i in range(int(rep)):
-            p=subprocess.run(cmd,capture_output=True,text=True,shell=True)
-            output += p.stdout
-            sleep(int(dur))
-        context = {'output':output}
+        result=runcommand(cmd,rep,dur)
+        context = {'output':result}
         return render(request,'home.html',context)
+
+    if 'celeryform' in request.POST:
+        command = request.POST.get('command')
+        rep = request.POST.get('rep')
+        dur = request.POST.get('dur')
+        cmd ='powershell -command '+command 
+        result=celery_run.delay(cmd,rep,dur)
+        context = {'output':result}
+        return render(request,'home.html',context)       
     return render(request,'home.html')
+
+def runcommand(cmd,rep,dur):
+    output=''
+    for i in range(int(rep)):
+        time.sleep(int(dur))
+        p=subprocess.run(cmd,capture_output=True,text=True,shell=True)
+        output += p.stdout
+    return output
