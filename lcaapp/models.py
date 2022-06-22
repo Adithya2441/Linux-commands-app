7a2343a0598e06828d142464dddfcfa62f675f80
Adithya S <84130203+Adithya2441@users.noreply.github.com>
2022-06-22 17:19:09 +0530

    models

--- lcaapp/models.py
+++ lcaapp/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class lc(models.Model):
+    cmd = models.CharField(max_length=100)
+    rep = models.IntegerField()
+    sleep = models.IntegerField()
+
+    def __str__(self) :
+        return str(self.id)
