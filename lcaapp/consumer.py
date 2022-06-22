02f6ba2f53b5b6ad76b1cc8b0a4a08198ab54283
Adithya S <84130203+Adithya2441@users.noreply.github.com>
2022-06-23 03:23:20 +0530

    minor

--- lcaapp/consumer.py
+++ lcaapp/consumer.py
@@ -1,4 +1,5 @@
 import json
+from logging.handlers import SocketHandler
 import re
 from channels.generic.websocket import WebsocketConsumer
 from .models import out
@@ -9,5 +10,5 @@ class ws_consumer(WebsocketConsumer):
         self.accept()
         while(True):
             res = out.objects.latest('id')
-            serialized_obj = serializers.serialize('json',[res,])
+            serialized_obj = serializers.serialize('json',[res])
             self.send(json.dumps({'output':serialized_obj}))
